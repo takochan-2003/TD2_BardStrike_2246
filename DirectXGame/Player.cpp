23bb934f95c3b589ffb5f0fe12b9c3214e7e3c19
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2023-11-02 17:14:12 +0900

    ゲームパッドを取得した

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,1 +1,13 @@
 ﻿#include "Player.h"
+
+void Player::Initialize() {}
+
+void Player::Update() { // ゲームパッドの状態を得る変数
+	XINPUT_STATE joyState;
+
+	// ゲームパッド状態取得、ゲームパッドが有効の場合if文が通る
+	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+	}
+}
+
+void Player::Draw() {}
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -8,6 +8,10 @@
 #include "ViewProjection.h"
 #include "WorldTransform.h"
 
+#include "Player.h"
+
+#include <memory>
+
 /// <summary>
 /// ゲームシーン
 /// </summary>
@@ -43,6 +47,8 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	std::unique_ptr<Player> player_;
 
 	/// <summary>
 	/// ゲームシーン用
