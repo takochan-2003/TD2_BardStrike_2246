bada6db0a46b346ca577b7e631adffa472af979c
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2023-11-22 14:33:02 +0900

    csvファイルのエラーを直した

--- DirectXGame/Item.cpp
+++ DirectXGame/Item.cpp
@@ -1,18 +1,18 @@
 ﻿#include "Item.h"
 #include <cassert>
 
-void Item::Initialize(Model* model,Vector3 position) {
+void Item::Initialize(Model* model, Vector3 position) {
 	// NULLポインタチェック
 	assert(model);
 	// 引数からデータを受け取る
 	model_ = model;
-	//ポジション
+	// ポジション
 	worldTransform_.translation_ = position;
 
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
 
-	 worldTransform_.UpdateMatrix();
+	worldTransform_.UpdateMatrix();
 }
 
 void Item::Update() { worldTransform_.UpdateMatrix(); }
